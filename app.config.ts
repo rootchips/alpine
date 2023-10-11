6496453fb19f2ea55358763875b5f1c598369bfe
Roslan Saidi <roslansaidi@ymail.com>
2023-10-11 12:24:54 +0800

    Change

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Chips',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
