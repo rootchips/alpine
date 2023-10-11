d27e727db8be2e35381932c645704b27fcc9510d
Roslan Saidi <roslansaidi@ymail.com>
2023-10-11 12:33:59 +0800

    Change

--- app.config.ts
+++ app.config.ts
@@ -13,7 +13,7 @@ export default defineAppConfig({
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'chips' // alt of the logo
       }
     },
     footer: {
